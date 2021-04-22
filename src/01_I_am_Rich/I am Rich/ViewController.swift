f4e2734b00ccd9576f587da7940da0997b31e1f2
Milovan Tomasevic <tomas.ftn.e2@gmail.com>
2021-04-22 22:45:22 +0200

    01_I_am_Rich

---
+++ src/01_I_am_Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Milovan Tomašević on 11/05/20.
+//  Copyright © 2020 Milovan Tomašević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
