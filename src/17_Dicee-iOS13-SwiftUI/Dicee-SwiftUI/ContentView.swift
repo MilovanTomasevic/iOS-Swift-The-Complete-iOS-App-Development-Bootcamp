f8f08bec613e040d52438fa51425664ba6c69ec3
Milovan Tomasevic <tomas.ftn.e2@gmail.com>
2021-04-22 23:10:22 +0200

    17_Dicee-iOS13-SwiftUI

---
+++ src/17_Dicee-iOS13-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Milovan Tomašević on 18/11/2020.
+//  Copyright © 2020 Milovan Tomašević. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
