a7dd41c847630f2b2d76d6fd7c7951b4d3dd6eda
Milovan Tomasevic <tomas.ftn.e2@gmail.com>
2021-04-22 22:48:59 +0200

    05_AutoLayout-iOS13

---
+++ src/05_AutoLayout-iOS13/AutoLayout-iOS13/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AutoLayout-iOS13
+//
+//  Created by Milovan Tomašević on 28/06/2020.
+//  Copyright © 2020 Milovan Tomašević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = allDice[Int.random(in: 0...5)]
+        diceImageView2.image = allDice[Int.random(in: 0...5)]
+        
+    }
+    
+}
+
