a51971ed81db32a4c186b36b0b501a6ce7ccb678
Milovan Tomasevic <tomas.ftn.e2@gmail.com>
2021-04-22 23:15:30 +0200

    21_Calculator-Advanced-Swift-iOS13-Completed

---
+++ src/21_Calculator-Advanced-Swift-iOS13-Completed/Calculator/Controller/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Milovan Tomašević on 27/11/2020.
+//  Copyright © 2020 Milovan Tomašević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    private var calculator = CalculatorLogic()
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+ 
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }
+        }
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
+    }
+
+}
+
---
+++ src/21_Calculator-Advanced-Swift-iOS13-Completed/Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,61 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Milovan Tomašević on 27/11/2020.
+//  Copyright © 2020 Milovan Tomašević. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String) -> Double? {
+       
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return performTwoNumCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil 
+    }
+    
+    private func performTwoNumCalculation(n2: Double) -> Double? {
+        
+        if let n1 = intermediateCalculation?.n1,
+            let operation = intermediateCalculation?.calcMethod {
+            
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases.")
+            }
+        }
+        return nil
+    }
+    
+}
